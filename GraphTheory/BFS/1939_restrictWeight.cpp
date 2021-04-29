ff650a717357db045ce834f8248acd71f09041c3
spqjf12345 <spqjf12345@gmail.com>
2021-04-29 19:22:15 +0900

    until 21.04.29

---
+++ GraphTheory/BFS/1939_restrictWeight.cpp
@@ -0,0 +1,85 @@
+//
+//  1939_restrictWeight.cpp
+//  SOMA👩🏻‍💻
+//
+//  Created by JoSoJeong on 2021/04/29.
+//
+
+
+///BFS with twoPointer
+#include <stdio.h>
+#include <iostream>
+#include <queue>
+#include <algorithm>
+#include <string.h>
+
+
+using namespace std;
+#define MAX 100001
+int maxCost = 0;
+vector<pair<int, int>> arr[MAX];
+int N, M;
+bool visit[MAX];
+int result_start, result_end;
+
+bool make_bfs(int mid_cost){
+    queue<int> q;
+    q.push(result_start);
+    visit[result_start] = true;
+    
+    while(q.size() != 0){
+        int now = q.front();
+        q.pop();
+        
+        if(now == result_end) {
+            return true;
+        }
+
+        for(int i =0; i < arr[now].size(); i++){
+            int next = arr[now][i].first;
+            int nextCost = arr[now][i].second;
+            if(visit[next] == false && mid_cost <= nextCost){
+                visit[next] = true;
+                q.push(next);
+            }
+        }
+        
+    }
+    
+    return false;
+}
+
+void bi_search(){
+    int left = 1;
+    int right = maxCost;
+    
+    while(left <= right){
+        memset(visit, false, sizeof(visit));
+        int mid = (left + right) / 2;
+        if(make_bfs(mid) == true){
+            left = mid + 1;
+        }else{
+            right = mid - 1;
+        }
+    }
+    cout << right << '\n';
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> N >> M;
+    for(int i = 0; i < M; i++){
+        int start, end, weight;
+        cin >> start >> end >> weight;
+        arr[start].push_back({end, weight}); //양방향 o
+        arr[end].push_back({start, weight});
+        maxCost = max(maxCost, weight);
+    }
+
+    cin >> result_start >> result_end;
+    
+    bi_search();
+    return 0;
+}
