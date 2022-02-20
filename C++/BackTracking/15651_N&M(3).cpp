3ce80013f8d4c171530e9506d7f66e455b9c0141
jouureee <jouureee@gmail.com>
2022-02-20 16:28:32 +0900

    Add N&M(3)

---
+++ C++/BackTracking/15651_N&M(3).cpp
@@ -0,0 +1,33 @@
+//
+//  15651_N&M(3).cpp
+//  SOMA👩🏻‍💻
+//
+//  Created by JoSoJeong on 2022/02/20.
+//
+
+#include <iostream>
+
+using namespace std;
+int N, M;
+int arr[8];
+
+void backTracking(int cnt){
+    if(cnt == M){ // M개 다 골랐다면
+        for(int i = 0; i < M; i++){
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    
+    for(int i = 1; i <= N; i++){
+        arr[cnt] = i;
+        backTracking(cnt+1);
+    }
+}
+
+int main(){
+    cin >> N >> M;
+    backTracking(0);
+    return 0;
+}
