9b500a6a89eaa792153d32f81739ebc27986f7ed
jouureee <jouureee@gmail.com>
2022-02-26 11:57:05 +0900

    not Int64

---
+++ Swift/Math/10757/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  SOMA👩🏻‍💻
+//
+//  Created by JoSoJeong on 2022/02/26.
+//
+
+import Foundation
+//var A: Int64 = 0
+//var B: Int64 = 0
+//
+//var input = readLine()!.split(separator: " ").map { Int64($0)! }
+//A = input[0]
+//B = input[1]
+//print(A + B)
+
